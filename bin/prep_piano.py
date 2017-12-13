8e249299b8cb59b0ea451241f3f625db7af8bca9
dmtr13 <dawirjowerdojo@gmail.com>
2017-12-13 02:03:02 +0100

    はくなまたた

--- bin/prep_piano.py
+++ bin/prep_piano.py
@@ -20,24 +20,36 @@ with open('../reference/proteinatlas.tsv', 'r') as en:
 ## -0.0819254141258643 0.93470602481617 0.999690735919201
 ### FOR DESEQ/2
 # os.popen("rm ../results/DESeq/*genesymb*")
-# dir = "../results/DESeq/"
-# for deseq2 in glob.glob((dir+"??_DESeq.tsv")):
-#     print ("Processing {}...".format(deseq2))
-#     newname = deseq2.replace(dir, "").replace("_DESeq.tsv","")
-#     newname += "_DESeq_genesymb.tsv"
-#     gs = open(dir+newname, 'w')
-#     with open(deseq2, 'r') as ds:
-#         ds = ds.readlines()
-#         gs.write("\"gene_symbol\""+'\t'+'\t'.join((ds[0].split())[1:]))
-#         for lines in ds[1:]:
-#             lines = lines.split()
-#             words = lines[1].replace("\"","")
-#             print (words)
-#             if words in gene_sym:
-#                 gs.write(gene_sym[words]+'\t'+'\t'.join(lines[2:])+'\n')
-#     gs.close()
+dir = "../results/DESeq/"
+for deseq2 in glob.glob((dir+"??_DESeq.tsv")):
+    print ("Processing {}...".format(deseq2))
+    newname = deseq2.replace(dir, "").replace("_DESeq.tsv","")
+    newname += "_DESeq_genesymb.tsv"
+    gs = open(dir+newname, 'w')
+    with open(deseq2, 'r') as ds:
+        ds = ds.readlines()
+        gs.write("\"gene_symbol\""+'\t'+'\t'.join((ds[0].split())[1:])+'\n')
+        for lines in ds[1:]:
+            lines = lines.split()
+            words = lines[1].replace("\"","")
+            if words in gene_sym:
+                gs.write(gene_sym[words]+'\t'+'\t'.join(lines[2:])+'\n')
+    gs.close()
 
 ### FOR LIMMA
+dir = "../results/Limma/"
+for limma in glob.glob((dir+"*_limma.tsv")):
+    print ("Processing {}...".format(limma))
+    newname = limma.replace(dir, "").replace("limma.tsv", "Limma_genesymb.tsv")
+    gs = open(dir+newname, 'w')
+    with open(limma, 'r') as L:
+        L = L.readlines()
+        gs.write("\"gene_symbol\""+'\t'+L[0]+'\n')
+        for lines in L[1:]:
+            lines = lines.split()
+            ensg = lines[0].replace("\"","")
+            if ensg in gene_sym:
+                gs.write(gene_sym[ensg]+'\t'+'\t'.join(lines[1:])+'\n')
 # gs = open("../results/Limma/Limma_genesymb.tsv", 'w')
 # with open("../results/Limma/Limma.tsv", 'r') as l:
 #     l = l.readlines()
@@ -49,6 +61,6 @@ with open('../reference/proteinatlas.tsv', 'r') as en:
 #         # print (ensg)
 #         if ensg in gene_sym:
 #             gs.write(gene_sym[ensg]+'\t'+'\t'.join(lines[1:])+'\n')
-# gs.close()
+    gs.close()
 
 print ("Done!")
